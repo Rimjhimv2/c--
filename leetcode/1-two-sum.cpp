36206764bedf9213250887e69a11268c463ce2cc
Rimjhim Verma <rimjhimv30@gmail.com>
2026-03-07 14:51:46 +0530

    [LeetSync] 1-two-sum — Easy

---
+++ leetcode/1-two-sum.cpp
@@ -0,0 +1,18 @@
+// Problem: 1. Two Sum
+// URL: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Language: 1#include <vector>
+// Date: 2026-03-07
+// Tags: Array, Hash Table
+// Synced by LeetSync Chrome Extension
+
+return {mp[complement], 
+i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
